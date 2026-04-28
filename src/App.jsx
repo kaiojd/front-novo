f813079c80e8fecfac0e1a27307cc4a879d81c12
KAIO JUNCKES DILLI <kaio_dilli@estudante.sesisenai.org.br>
2026-04-28 12:13:47 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ const redenrizartela = () => {
+  //navegação
+ }
+
+  return (
+    <>
+      <section id="center">
+      
+       </section>
+    </>
+  )
+}
+
+export default App
