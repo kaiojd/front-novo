7d4f9bd3f5f13821bbad0febebbf084c5db71914
KAIO JUNCKES DILLI <kaio_dilli@estudante.sesisenai.org.br>
2026-04-30 11:50:56 -0300

    telas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './paginas/Login'
+import Cadastro from './paginas/Cadastro'
+import Usuario from './paginas/Usuario'
 
 function App() {
- const redenrizartela = () => {
-  //navegação
- }
+  const [tela, setTela ] = useState('login')
+
+ const renderizartela = () => {
+  if(tela === 'login') {
+    return <Login/>
+  } else if(tela === 'cadastro') {
+    return <Cadastro/>
+  }
+  else if(tela === 'usuario') {
+    return <Usuario/>
+  }
+}
+
+const trocarTela = (pagina) => {
+  setTela(pagina)
+}
 
   return (
     <>
-      <section id="center">
-      
-       </section>
+      <h1>aaa</h1>
+      <button onClick={ () => trocarTela('login')}>login</button>
+      <button onClick={ () => trocarTela('cadastro')}>cadastro</button>
+      <button onClick={ () => trocarTela('usuario')}>usuario</button>
+      {renderizartela()}  
     </>
   )
 }
