2a58f145f318bc0009441390de1c78494e4cec79
KAIO JUNCKES DILLI <kaio_dilli@estudante.sesisenai.org.br>
2026-05-19 12:36:14 -0300

    nome senha

--- src/paginas/Usuario.jsx
+++ src/paginas/Usuario.jsx
@@ -3,12 +3,15 @@ import { useEffect, useState } from "react"
 
 export default function Usuario() {
     const [contador, setContador] = useState(0)
+     const [email, setEmail] = useState('')
     const [usuarios, setUsuarios] = useState([])
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
 useEffect(() => {
     document.title = 'contagem ' + contador;
     const buscarUsuarios = async () => {
         const resposta = await fetch('http://localhost:3000/usuarios')
-        const usuarios = await resposta.json()
+        const data = await resposta.json()
         setUsuarios(data)
         console.log(data);
     }
@@ -25,13 +28,38 @@ useEffect(() => {
                 somar
             </button>
 
+            <input type = "text" id="email"
+            placeholder="DIGITE EMAIL"
+            value={email}
+            onChange = {(e) => setEmail(e.target.value)}
+
+            />
+
+               <input type = "text" id="nome"
+            placeholder="DIGITE SEU NOME"
+            value={nome}
+            onChange = {(e) => setNome(e.target.value)}
+
+            />
+
+            <input type = "text" id="senha"
+            placeholder="DIGITE SUA SENHA"
+            value={senha}
+            onChange = {(e) => setSenha(e.target.value)}
+
+            />
+
+            <h3>{email}</h3>
+            <h3>{nome}</h3>
+            <h3>{senha}</h3>
+
         <ul>
-            {usuarios.map( (usuario) => {
+            {usuarios.map( (usuario) => (
                 <li key={usuario.id}>
                     <b>{usuario.nome}</b>
                     STATUS: {usuario.ativo ? 'ativo' : 'desativo'}
                 </li>
-            })}
+            ))}
         </ul>
         </div>
       
